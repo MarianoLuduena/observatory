693957cd5db7ba6f769816f1c28ad1c0c9b0d616
MarianoLuduena <marianogabriel.house@gmail.com>
2018-04-08 19:28:18 -0300

    Station domain class

---
+++ src/main/scala/observatory/domain/Station.scala
@@ -0,0 +1,31 @@
+package observatory.domain
+
+import observatory.Location
+import observatory.parsers.CSVLineReader
+
+import scala.util.Try
+
+object Station {
+
+  /**
+    * CSV Format: "STN Identifier, WBAN Identifier, Latitude, Longitude"
+    *
+    * @param line String representing a line from the CSV input file
+    * @return Try[Station]
+    */
+  def parse(line: String): Try[Station] = Try {
+    val lr = new CSVLineReader(line)
+    Station(
+      stn = lr.readStr,
+      wban = lr.readStr,
+      location = Location(lr.readDouble, lr.readDouble)
+    )
+  }
+}
+
+case class Station(stn: String, wban: String, location: Location) {
+
+  val uid: String = stn + wban
+
+  val toPair: (String, Station) = (uid, this)
+}
