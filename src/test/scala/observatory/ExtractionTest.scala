1c4bbedc5062a202886cb3b28732ceb9aeac5ff9
MarianoLuduena <marianogabriel.house@gmail.com>
2018-04-09 00:56:07 -0300

    Basic test of milestone 1
    I am mostly interested in potential serialization/performance problems.
    I will rely on the grader to make sure that functional requirements are
    fulfilled.

--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -6,5 +6,15 @@ import org.scalatest.junit.JUnitRunner
 
 trait ExtractionTest extends FunSuite {
 
-  
+  ignore("Extraction with 2015 Dataset") {
+    val seq = Extraction.locateTemperatures(
+      year = 2015,
+      stationsFile = "./src/main/resources/stations.csv",
+      temperaturesFile = "./src/main/resources/2015.csv"
+    )
+    assertResult(4091191, "Sequence size does not match")(seq.size)
+
+    val avgTempByLocation = Extraction.locationYearlyAverageRecords(seq).take(100)
+    avgTempByLocation.foreach(println)
+  }
 }
