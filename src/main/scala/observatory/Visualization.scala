174589e9c3ac4ad89ba1d715548a0087ce9b1e16
MarianoLuduena <marianogabriel.house@gmail.com>
2018-04-12 00:49:24 -0300

    Milestone 2 completed
    Code has yet to be submitted

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,11 +1,12 @@
 package observatory
 
-import com.sksamuel.scrimage.{Image, Pixel}
+import com.sksamuel.scrimage.Image
+import observatory.helpers.InterpolationHelper
 
 /**
   * 2nd milestone: basic visualization
   */
-object Visualization {
+object Visualization extends InterpolationHelper {
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -13,7 +14,8 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+    val samples = temperatures.par.map(t => (gcDistance(t._1, location), t._2))
+    idw(samples)
   }
 
   /**
@@ -22,7 +24,14 @@ object Visualization {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
-    ???
+    val (lowerPoints, upperPoints) = points.toArray.sortBy(_._1).partition(_._1 < value)
+    (lowerPoints.lastOption, upperPoints.headOption) match {
+      case (_, Some(up)) if up._1 == value => up._2
+      case (Some(lp), Some(up)) => interpolateColor(lp, up, value)
+      case (Some(lp), _) => lp._2
+      case (_, Some(up)) => up._2
+      case _ => Color(0, 0, 0)  // black
+    }
   }
 
   /**
@@ -31,7 +40,22 @@ object Visualization {
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
-    ???
+    val width: Int = 360
+    val height: Int = 180
+
+    def posToLocation(width: Int, height: Int, pos: Int): Location = {
+      val latitude = 90 - 180.0 / height * (pos / width)
+      val longitude = -180 + 360.0 / width * (pos % width)
+      Location(lat = latitude, lon = longitude)
+    }
+
+    val pixels = (0 until (width * height)).toParArray.map { x =>
+      val predictedTemperature = predictTemperature(temperatures, posToLocation(width, height, x))
+      val interpolatedColour = interpolateColor(colors, predictedTemperature)
+      interpolatedColour.toPixel()
+    }
+
+    Image(width, height, pixels.toArray)
   }
 
 }
---
+++ src/main/scala/observatory/helpers/InterpolationHelper.scala
@@ -0,0 +1,107 @@
+package observatory.helpers
+
+import observatory.{Color, Location}
+
+import scala.collection.GenIterable
+import scala.math._
+
+trait InterpolationHelper {
+
+  /**
+    * Power parameter p.
+    * Greater values of p assign greater influence to values closest to the interpolated point.
+    * For N dimensions, power parameters p <= N cause the interpolated values to be dominated by points far away.
+    *
+    * @return Power parameter p value
+    */
+  protected def p: Double = 4
+
+  /**
+    * Inverse distance weighting
+    *
+    * @param samples List of values (distance, value -temperature-)
+    * @return Interpolated value
+    */
+  protected def idw(samples: GenIterable[(Double, Double)]): Double = {
+    val (num, denom, _) = samples.par.aggregate((0.0, 0.0, false))({ (ndl, s) =>
+      if (ndl._3) ndl
+      else if (s._1 < 1) (s._2, 1.0, true)
+      else {
+        val w = 1 / pow(s._1, p)
+        (ndl._1 + s._2 * w, ndl._2 + w, ndl._3)
+      }
+    }, {
+      case (t1@(_, _, l1), _) if l1 => t1
+      case (_, t2@(_, _, l2)) if l2 => t2
+      case ((n1, d1, _), (n2, d2, _)) => (n1 + n2, d1 + d2, false)
+    })
+
+    num / denom
+  }
+
+  /**
+    * Method to determine whether one point is the antipodes of another one
+    *
+    * @param p1 Latitude and longitude of the first location
+    * @param p2 Latitude and longitude of the second location
+    * @return True if p2 is the antipodes of p1; false otherwise
+    */
+  protected def isAntipodes(p1: Location, p2: Location): Boolean =
+    p1.lat == -p2.lat && (p1.lon == p2.lon + 180 || p1.lon == p2.lon - 180)
+
+  /**
+    * Earth's radius
+    *
+    * @return Earth's mean radius in Kilometers (Km)
+    */
+  protected def radius = 6371.0088
+
+  /**
+    * Great-circle distance
+    *
+    * @param p1 First location (point 1)
+    * @param p2 Second location (point 2)
+    * @return Distance between locations in Kilometers (Km)
+    */
+  protected def gcDistance(p1: Location, p2: Location): Double = {
+    val centralAngle =
+      if (p1 == p2) 0
+      else if (isAntipodes(p1, p2)) Pi
+      else {
+        val p1Lat = p1.lat.toRadians
+        val p2Lat = p2.lat.toRadians
+        val deltaLon = abs(p1.lon.toRadians - p2.lon.toRadians)
+        acos(sin(p1Lat) * sin(p2Lat) + cos(p1Lat) * cos(p2Lat) * cos(deltaLon))
+      }
+
+    radius * centralAngle
+  }
+
+  /**
+    * Linear interpolation between two known points
+    *
+    * @param x0 X coordinate of first point
+    * @param y0 Y coordinate of first point
+    * @param x1 X coordinate of second point
+    * @param y1 Y coordinate of second point
+    * @param x Position at which I want to find the interpolated value
+    * @return Interpolated value (Double) y
+    */
+  protected def lerp(x0: Double, y0: Int, x1: Double, y1: Int, x: Double): Double =
+    (y0 * (x1 - x) + y1 * (x - x0)) / (x1 - x0)
+
+  /**
+    * Method to determine the interpolated colour given to known points with their associated colours
+    *
+    * @param lp Lower point (Temperature, Color)
+    * @param up Upper point (Temperature, Color)
+    * @param x Temperature for which the Color is desired
+    * @return The interpolated Color for x Temperature
+    */
+  protected def interpolateColor(lp: (Double, Color), up: (Double, Color), x: Double): Color =
+    Color(
+      red = lerp(lp._1, lp._2.red, up._1, up._2.red, x).toInt,
+      green = lerp(lp._1, lp._2.green, up._1, up._2.green, x).toInt,
+      blue = lerp(lp._1, lp._2.blue, up._1, up._2.blue, x).toInt
+    )
+}
