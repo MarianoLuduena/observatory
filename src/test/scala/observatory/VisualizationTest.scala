3073d48492cff49174a3d5d477f12ccc5d7a6342
MarianoLuduena <marianogabriel.house@gmail.com>
2018-04-12 00:53:16 -0300

    Visualization test with 2015's dataset
    The test takes about 4.5 minutes to complete (running on an i5 processor
    with 8Gb RAM)

--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -6,5 +6,27 @@ import org.scalatest.prop.Checkers
 
 trait VisualizationTest extends FunSuite with Checkers {
 
+  private val temperaturesColourScale = Iterable[(observatory.Temperature, Color)](
+    (60.0, Color(255, 255, 255)),
+    (32.0, Color(255, 0, 0)),
+    (12.0, Color(255, 255, 0)),
+    (0.0, Color(0, 255, 255)),
+    (-15.0, Color(0, 0, 255)),
+    (-27.0, Color(255, 0, 255)),
+    (-50.0, Color(33, 0, 107)),
+    (-60.0, Color(0, 0, 0))
+  )
 
+  ignore("2015 temperatures image to disk") {
+    val avgTempByLocation = {
+      val seq = Extraction.locateTemperatures(
+        year = 2015,
+        stationsFile = "/stations.csv",
+        temperaturesFile = "/2015.csv"
+      )
+      Extraction.locationYearlyAverageRecords(seq)
+    }
+
+    Visualization.visualize(avgTempByLocation, temperaturesColourScale).output("/tmp/test.png")
+  }
 }
