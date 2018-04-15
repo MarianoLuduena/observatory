dfe5a4778b3e2321ce0e402cb331ddf17a87f735
MarianoLuduena <marianogabriel.house@gmail.com>
2018-04-15 00:07:05 -0300

    1975's temperatures tile method test in InteractionTest

--- src/test/scala/observatory/InteractionTest.scala
+++ src/test/scala/observatory/InteractionTest.scala
@@ -7,4 +7,30 @@ import scala.collection.concurrent.TrieMap
 
 trait InteractionTest extends FunSuite with Checkers {
 
+  private val temperaturesColourScale = Iterable[(observatory.Temperature, Color)](
+    (60.0, Color(255, 255, 255)),
+    (32.0, Color(255, 0, 0)),
+    (12.0, Color(255, 255, 0)),
+    (0.0, Color(0, 255, 255)),
+    (-15.0, Color(0, 0, 255)),
+    (-27.0, Color(255, 0, 255)),
+    (-50.0, Color(33, 0, 107)),
+    (-60.0, Color(0, 0, 0))
+  )
+
+  ignore("One tile") {
+    val img = {
+      val avgTemp = Extraction.locationYearlyAverageRecords(
+        Extraction.locateTemperatures(
+          year = 1975,
+          stationsFile = "/stations.csv",
+          temperaturesFile = "/1975.csv"
+        )
+      )
+      Interaction.tile(avgTemp, temperaturesColourScale, Tile(0, 0, 0))
+    }
+
+    val path = img.output("/tmp/test.png")
+    println(path)
+  }
 }
