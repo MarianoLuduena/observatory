4c8ca120c2d376e0ff1ddce80ae7fb740f29ad06
MarianoLuduena <marianogabriel.house@gmail.com>
2018-04-17 19:54:23 -0300

    Simple test for calculating averages with RDDs

--- src/test/scala/observatory/ManipulationTest.scala
+++ src/test/scala/observatory/ManipulationTest.scala
@@ -5,4 +5,20 @@ import org.scalatest.prop.Checkers
 
 trait ManipulationTest extends FunSuite with Checkers {
 
+  ignore("Average with RDDs") {
+    val tempsByYear = (1975 to 1980).map { year =>
+      val temperaturesRDD = Extraction.locateTemperaturesRDD(
+        year,
+        stationsFile = "/stations.csv",
+        temperaturesFile = s"/$year.csv"
+      )
+      val avgTemperaturesRDD = Extraction.locationYearlyAverageRecordsRDD(temperaturesRDD)
+      temperaturesRDD.unpersist(blocking = false)
+      avgTemperaturesRDD
+    }
+
+    val fn = Manipulation.averageRDD(tempsByYear)
+    println(s"(0, 0) = ${fn(GridLocation(0, 0))}")
+  }
+
 }
