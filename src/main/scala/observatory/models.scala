8337d95de16c23a1d5de7f77bf3712971834e519
MarianoLuduena <marianogabriel.house@gmail.com>
2018-04-17 23:01:57 -0300

    ParArray replaced by Array
    The grader doesn't like mutable collections

--- src/main/scala/observatory/models.scala
+++ src/main/scala/observatory/models.scala
@@ -1,7 +1,6 @@
 package observatory
 
 import com.sksamuel.scrimage.Pixel
-import scala.collection.parallel.mutable.ParArray
 import scala.math.{Pi, atan, pow, sinh}
 
 /**
@@ -78,9 +77,9 @@ sealed class Grid {
   val height: Int = 180
   val size: Int = width * height
 
-  private val data: ParArray[Temperature] = new ParArray[Temperature](size)
+  private val data: Array[Temperature] = new Array[Temperature](size)
 
-  def setValueAt(x: Int, y: Int, value: Temperature): Unit = data(x + y * width) = value
+  def setValueAt(x: Int, y: Int, value: Temperature): Unit = data.update(x + y * width, value)  // data(x + y * width) = value
 
   /**
     * The latitude can be any integer between -89 and 90, and the longitude can be any integer between -180 and 179.
@@ -97,9 +96,9 @@ sealed class Grid {
   }
 
   def +=(that: Grid): Grid = {
-    (0 until size).par.foreach(i => data(i) += that.data(i))
+    (0 until size).par.foreach(i => data.update(i, data(i) + that.data(i)))
     this
   }
 
-  def scaleTo(factor: Double): Unit = (0 until size).par.foreach(i => data(i) *= factor)
+  def scaleTo(factor: Double): Unit = (0 until size).par.foreach(i => data.update(i, data(i) * factor))
 }
