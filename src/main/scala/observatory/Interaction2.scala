9111d1ca23e78f9a08771b40ffd34744a1f27e2c
MarianoLuduena <marianogabriel.house@gmail.com>
2018-04-22 17:52:12 -0300

    Milestone six completed
    Grader's output:
    
    "Your overall score for this assignment is 10.00 out of 10.00
    
    Your solution passed all of our tests, congratulations! You obtained the
    maximal test
    score of 9.00.
    
    Our automated style checker tool could not find any issues with your
    code. You obtained the maximal
    style score of 1.00."

--- src/main/scala/observatory/Interaction2.scala
+++ src/main/scala/observatory/Interaction2.scala
@@ -5,20 +5,41 @@ package observatory
   */
 object Interaction2 {
 
+  private val temperaturesColourScale: Seq[(Temperature, Color)] = Seq(
+    (60.0, Color(255, 255, 255)),
+    (32.0, Color(255, 0, 0)),
+    (12.0, Color(255, 255, 0)),
+    (0.0, Color(0, 255, 255)),
+    (-15.0, Color(0, 0, 255)),
+    (-27.0, Color(255, 0, 255)),
+    (-50.0, Color(33, 0, 107)),
+    (-60.0, Color(0, 0, 0))
+  )
+
+  private val deviationsColourScale: Seq[(Temperature, Color)] = Seq(
+    (7.0, Color(0, 0, 0)),
+    (4.0, Color(255, 0, 0)),
+    (2.0, Color(255, 255, 0)),
+    (0.0, Color(255, 255, 255)),
+    (-2.0, Color(0, 255, 255)),
+    (-7.0, Color(0, 0, 255))
+  )
+
   /**
     * @return The available layers of the application
     */
   def availableLayers: Seq[Layer] = {
-    ???
+    Seq[Layer](
+      Layer(LayerName.Temperatures, temperaturesColourScale, 1990 to 2015),
+      Layer(LayerName.Deviations, deviationsColourScale, 1990 to 2015)
+    )
   }
 
   /**
     * @param selectedLayer A signal carrying the layer selected by the user
     * @return A signal containing the year bounds corresponding to the selected layer
     */
-  def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = {
-    ???
-  }
+  def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = Signal(selectedLayer().bounds)
 
   /**
     * @param selectedLayer The selected layer
@@ -28,8 +49,9 @@ object Interaction2 {
     *         this method should return the closest value that is included
     *         in the `selectedLayer` bounds.
     */
-  def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Year]): Signal[Year] = {
-    ???
+  def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Year]): Signal[Year] = Signal {
+    val range = selectedLayer().bounds
+    sliderValue().max(range.start).min(range.end)
   }
 
   /**
@@ -37,8 +59,8 @@ object Interaction2 {
     * @param selectedYear The selected year
     * @return The URL pattern to retrieve tiles
     */
-  def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
+  def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = Signal {
+    s"target/${selectedLayer().layerName.id}/${selectedYear()}/{z}/{x}-{y}.png"
   }
 
   /**
@@ -46,13 +68,16 @@ object Interaction2 {
     * @param selectedYear The selected year
     * @return The caption to show
     */
-  def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
+  def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = Signal {
+    s"${selectedLayer().layerName.camelCaseId} (${selectedYear()})"
   }
 
 }
 
-sealed abstract class LayerName(val id: String)
+sealed abstract class LayerName(val id: String) {
+
+  val camelCaseId: String = id.head.toUpper + id.tail
+}
 object LayerName {
   case object Temperatures extends LayerName("temperatures")
   case object Deviations extends LayerName("deviations")
