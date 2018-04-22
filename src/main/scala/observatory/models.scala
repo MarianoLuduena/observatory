9282e6f2f6b8c8aaec8e522f6902d6eaf50fa223
MarianoLuduena <marianogabriel.house@gmail.com>
2018-04-22 17:50:59 -0300

    Grid moved out from models.scala

--- src/main/scala/observatory/models.scala
+++ src/main/scala/observatory/models.scala
@@ -1,6 +1,5 @@
 package observatory
 
-import com.sksamuel.scrimage.Pixel
 import scala.math.{Pi, atan, pow, sinh}
 
 /**
@@ -50,55 +49,4 @@ case class CellPoint(x: Double, y: Double)
   * @param green Level of green, 0 ≤ green ≤ 255
   * @param blue Level of blue, 0 ≤ blue ≤ 255
   */
-case class Color(red: Int, green: Int, blue: Int) {
-
-  def toPixel(alphaChannel: Int = 255): Pixel = Pixel(red, green, blue, alphaChannel)
-}
-
-object Grid {
-
-  def apply(temperatures: Iterable[(Location, Temperature)]): Grid = {
-    val grid: Grid = new Grid
-
-    (0 until grid.width).par.foreach { x =>
-      (0 until grid.height).par.foreach { y =>
-        val location: Location = Location(lat = grid.height / 2 - y, lon = x - grid.width / 2)
-        grid.setValueAt(x, y, Visualization.predictTemperature(temperatures, location))
-      }
-    }
-
-    grid
-  }
-}
-
-sealed class Grid {
-
-  val width: Int = 360
-  val height: Int = 180
-  val size: Int = width * height
-
-  private val data: Array[Temperature] = new Array[Temperature](size)
-
-  def setValueAt(x: Int, y: Int, value: Temperature): Unit = data.update(x + y * width, value)  // data(x + y * width) = value
-
-  /**
-    * The latitude can be any integer between -89 and 90, and the longitude can be any integer between -180 and 179.
-    * The top-left corner has coordinates (90, -180), and the bottom-right corner has coordinates (-89, 179).
-    *
-    * @param gl GridLocation
-    * @return The Temperature at the given GridLocation
-    */
-  def getTemperature(gl: GridLocation): Temperature = {
-    val x: Int = gl.lon + width / 2
-    val y: Int = height / 2 - gl.lat
-
-    data(x + y * width)
-  }
-
-  def +=(that: Grid): Grid = {
-    (0 until size).par.foreach(i => data.update(i, data(i) + that.data(i)))
-    this
-  }
-
-  def scaleTo(factor: Double): Unit = (0 until size).par.foreach(i => data.update(i, data(i) * factor))
-}
+case class Color(red: Int, green: Int, blue: Int)
